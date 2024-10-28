a50f5230bbfc49f683504fd27147400dc3212773
Eugenio Paolantonio (g7) <me@medesimo.eu>
2024-10-28 21:33:57 +0100

    [droidian-snapshot] Inject droidian-gnome-settings-daemon:next-upgrade-47
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- droidian-snapshot.mk
+++ droidian-snapshot.mk
@@ -11,6 +11,7 @@ DROIDIAN_UPDATE_FROM = 99
 DROIDIAN_FEATURE_BRANCHES = \
  droidian-gnome-clocks:next-upgrade-47 \
  droidian-gnome-control-center:next-upgrade-47-rebase \
+ droidian-gnome-settings-daemon:next-upgrade-47 \
  droidian-network-manager:next-wwan-fix \
  droidian-firefox-esr-mobile-config:next-firefox \
  droidian-glib:next-pidfd \
