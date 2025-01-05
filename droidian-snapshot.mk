7f1db92cdf2fb6f896db3cc532ac4c252ad5bc0e
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-01-05 16:06:12 +0100

    [droidian-snapshot] Bump Debian snapshot to 20250105T085122Z
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- droidian-snapshot.mk
+++ droidian-snapshot.mk
@@ -1,7 +1,7 @@
 # Control file for the current Droidian snapshot.
 
 # The base Debian snapshot
-DEBIAN_SNAPSHOT = 20241221T084728Z
+DEBIAN_SNAPSHOT = 20250105T085122Z
 
 # The base Debian suite
 DEBIAN_SUITE = sid
