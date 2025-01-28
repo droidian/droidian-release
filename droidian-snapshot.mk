9655ecdf7f51a6b6ccbb25de1b26949e496ec005
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-01-28 01:49:14 +0100

    [droidian-snapshot] Drop droidian-ofono2mm:next-gnumdk: now merged
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- droidian-snapshot.mk
+++ droidian-snapshot.mk
@@ -31,7 +31,6 @@ DROIDIAN_FEATURE_BRANCHES = \
  droidian-glib:next-pidfd \
  droidian-gtk4:next-droidian-4-16 \
  droidian-gmobile:next-upgrade-0-2 \
- droidian-ofono2mm:next-gnumdk \
  droidian-hadess-sensorfw-proxy:next-testing \
  droidian-gnome-initial-setup:next-droidian-47 \
  droidian-wlroots:next-upgrade-0-17-4 \
