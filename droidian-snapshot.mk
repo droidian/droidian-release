6b913649f436d4d74dedeac66087ab9ac46beff8
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-04-13 17:06:26 +0200

    [droidian-snapshot] Inject droidian-feedbackd:next-upgrade-0-8
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- droidian-snapshot.mk
+++ droidian-snapshot.mk
@@ -23,6 +23,7 @@ DROIDIAN_UPDATE_FROM = 100
 # Droidian feature branches to inject
 DROIDIAN_FEATURE_BRANCHES = \
  droidian-geoclue:next-2-7-2 \
+ droidian-feedbackd:next-upgrade-0-8 \
  droidian-systemd:next-upgrade-257 \
  droidian-gnome-clocks:next-upgrade-47 \
  droidian-gnome-control-center:next-upgrade-47-rebase \
