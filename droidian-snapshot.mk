d68d450894392e1e3e521a1a057964ebf43cc1a8
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-10-13 02:59:58 +0200

    [droidian-snapshot] Add droidian-qt6-base:next-gles
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- droidian-snapshot.mk
+++ droidian-snapshot.mk
@@ -32,6 +32,7 @@ DROIDIAN_FEATURE_BRANCHES = \
  droidian-gnome-control-center:next-upgrade-47-rebase \
  droidian-network-manager:next-wwan-fix \
  droidian-firefox-esr-mobile-config:next-firefox \
+ droidian-qt6-base:next-gles \
  droidian-glib:next-pidfd-286 \
  droidian-gtk-3.0:next-upgrade-3-24 \
  droidian-gmobile:next-upgrade-0-4 \
