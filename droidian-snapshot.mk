430249dc2c3fbd9767b469da6e02b745d11129d9
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-11-17 23:44:23 +0100

    [droidian-snapshot] Add plasma-6-5 group

--- droidian-snapshot.mk
+++ droidian-snapshot.mk
@@ -48,4 +48,5 @@ DROIDIAN_FEATURE_BRANCHES = \
 DROIDIAN_GROUPS = \
  droidian:next-phosh-0-47 \
  droidian:next-phosh-0-49 \
- droidian:next-gnome-48
+ droidian:next-gnome-48 \
+ droidian:next-plasma-6-5
